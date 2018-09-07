38379f94d3ec496c1e8e370a8d342d40633efd70
Vandan Agarwal <vandanrkt@gmail.com>
2018-09-08 03:21:40 +0530

    ZcxvZv

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 	<head>
     <!-- Bootstrap Core CSS -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/event.css" rel="stylesheet">
     <link href="css/card.css" rel="stylesheet">
@@ -9,6 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.css" />
     <script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.js"></script>
+   <link href="https://fonts.googleapis.com/css?family=Archivo+Narrow|Crete+Round|Kanit|Monoton|Play|Ubuntu+Condensed" rel="stylesheet">
     <link href="css/header.css" rel="stylesheet">
     <link href="css/slider.css" rel="stylesheet">
     <link href="css/index.css" rel="stylesheet">
@@ -17,94 +19,10 @@
 </head>
 
 <style>
-h3{
-  color: #5e5d5d!important;
+.carousel-caption{
+	bottom: 0;
 }
-
-.panel-body ul{
-    /*color: red;*/
-    color: #463e1e;
-    padding-left: 2px;
-    margin-left: 0px;
-    margin-top: -10px;
-    /*padding-bottom: 15px;*/
-}
-
-.panel-default{
-  border-radius: 0px!important;
-}
-.panel-body {
-    text-align: justify;
-}
-.panel-heading{
-  color: red!important;
-  /*font-weight: bold;*/
-}
-.carousel-control {
-  padding-top:10%;
-  width:5%!important;
-
-  /*background-color: red;*/
-}
-.carousel-indicators{
-  /*background-color: red!important;*/
-  /*margin-bottom: -10px!important;*/
-}
-.carousel-control.left, .carousel-control.right {
-    background-image: none
-}
-
-.glyphicon-chevron-right:hover{
-     display:block!important;
-}
-.carousel-caption p{
-  background-color:rgba(255, 255, 255, 0.5)!important;
-  height: 35px!important;
-  margin-left: 0px!important;
-  color: #022d10;
-  /*font-style: italic;*/
-  font-weight: bold;
-  text-align: left;
-  padding-left: 10px!important;
-  padding-top: 8px!important;
-  font-family: 'Sintony';
-  font-size: 1.1em;
-  
-}
-.slider{
-  min-height: 400px;
-  width: 1200px;
-  /*margin-bottom: 50px;*/
-}
-
-.panel-footer{
-  /*background-color: LightGray!important;*/
-  font-weight: bold;
-}
-.carousel-control.left{
-  background-image: none!important;
-}
-
-.carousel-control.right {
-  background-image: none!important;
-}
-
-.carousel-control.left, .carousel-control.right:hover {
-  background-image: none;
-}
-.img-thumbnail{
-  border-radius: 0px!important;
-}
-.myImg{
-  /*background-color: red!important;*/
-  margin-bottom: 10px;
-  
-  /*margin-right: 20px;*/
-  /*width:550px!important;*/
-  height: 100px!important;
-  /*border-radius: 5px!important;*/
-}
-
+ 
 </style>
 
 
@@ -118,43 +36,42 @@ h3{
     <?php include('header.php') ; ?>
     <div class="container" style="background-color:white;">
 
-    <div class="row">
+    <div class="row" >
             <div class="col-md-12 col-sm-12 col-xs-12">
               <?php include('menu1.php'); ?>
-
-            <div id="myCarousel" class="carousel slide" data-ride="carousel" style="margin-top:10px;" >
-                 
+              
+    		   <div id="myCarousel" class="carousel slide" data-ride="carousel" style="margin-top:10px;" >
                   <div class="carousel-inner my-slider" role="listbox">
                     <div class="item active">
-                      <img src="images/slider_img/cseiiti1.jpg" class="slider img-thumbnail">
-                       <div class="carousel-caption" >
+                      <img src="images/slider_img/cseiiti1.jpg" class="slider img-thumbnail" style="height: 100%;">
+                       <div class="carousel-caption" style="left: 5%; width: 90%; bottom: 4px;" >
                        <p>First Caption</p>
                       </div>
                     </div>
 
-                    <div class="item">
-                      <img src="images/slider_img/cseiiti2.jpg" class="slider img-thumbnail">
-                      <div class="carousel-caption">
+                    <div class="item" >
+                      <img src="images/slider_img/cseiiti2.jpg" class="slider img-thumbnail" style="height: 100%;">
+                      <div class="carousel-caption" style="left: 5%; width: 90%; bottom: 4px;">
                        <p>Second Caption</p>
                       </div>
                     </div>
 
                     <div class="item">
-                      <img src="images/slider_img/cseiiti3.jpg" class="slider img-thumbnail">
-                      <div class="carousel-caption">
+                      <img src="images/slider_img/cseiiti3.jpg" class="slider img-thumbnail" style="height: 100%; bottom: 0;">
+                      <div class="carousel-caption" style="left: 5%; width: 90%; bottom: 4px;">
                        <p>Third Caption</p>
                       </div>
                     </div>
 
                     <div class="item">
-                      <img src="images/slider_img/cseiiti4.jpg" class="slider img-thumbnail">
-                      <div class="carousel-caption">
+                      <img src="images/slider_img/cseiiti4.jpg" class="slider img-thumbnail" style="height: 100%;">
+                      <div class="carousel-caption" style="left: 5%; width: 90%; bottom: 4px;">
                        <p>Fourth Caption</p>
                       </div>
                     </div>
             </div>
 
-  <!-- Left and right controls -->
+  
                 <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
                   <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                   <span class="sr-only">Previous</span>
@@ -164,10 +81,12 @@ h3{
                   <span class="sr-only">Next</span>
                 </a>
               </div>
+          </div>
+        </div>
+   
 
-        </div>
-            
-        </div>
+
+
 
 
         <div class="row" style="margin-top:20px;">
@@ -194,10 +113,7 @@ h3{
                    
                  </div>
 
-                 
-
-
-          </div>
+         </div>
         </div>
   <div class="row" style="margin-top:5px;">
         <div class="col-md-12 col-sm-12 col-xs-12"> 
@@ -467,7 +383,9 @@ h3{
           </div>
 
 <?php include('footer.php') ; ?>
+
 </body>
+
 
 </html>
 
