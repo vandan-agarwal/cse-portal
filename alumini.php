9b1bed078120d92839455044655afd08750c8c59
Vandan Agarwal <vandanrkt@gmail.com>
2018-09-17 11:43:44 +0530

    the website is finally rocking

--- alumini.php
+++ alumini.php
@@ -13,7 +13,7 @@
   font-family: Arial, Helvetica, sans-serif;
   margin-top: 15px;
   margin:auto;
-  width:1200;  
+  width:1155px;  
   color: #013953;
   border-collapse:collapse;
 }
@@ -41,120 +41,61 @@
         <?php include('menu1.php'); ?>
 
 <h2 style="font-family: 'Audiowide', cursive; color: #013953; text-shadow:2px 2px #bdcfdb; text-align: center;" >Graduated Ph. D. Students</h2>
-<div id="inner_content">
+ <div id="inner_content">
 <div class="container table-responsive" style="margin-top: 15px;">
 <table id="alumini-list">
-<tr><th>S.No. </th><th>Name </th><th>Thesis </th><th>Title  </th><th>Month/Year of Graduation</th></tr>
-<tr><td>Algorithms and Complexity               </td> <td> 
-<a href="http://iiti.ac.in/people/~narendra/" target="_newTab">Dr. Narendra S. Chaudhari</a> 
-</td></tr>
-<tr><td>Theoretical Computer Science            </td> <td>                          </td></tr>
-<tr><td>Combinatorics: Design Theory            </td> <td>                          </td></tr>
-<tr><td>			                            </td> <td>                          </td></tr>
+<tr>
+  <th>S/N&nbsp;</th>
+  <th>Name </th>
+  <th>Thesis Title </th>
+  <th>Month/Year of Graduation</th>
+</tr>
+<tr>
+  <td>1.</td>
+  <td> Jaya Thomas</td>
+  <td>  Approaches for Cutting Stock and Strip Packing Problems </td>
+  <td>2014</td>
+</tr>
+<tr>
+  <td>2.</td>
+  <td> Neetesh Saxena</td>
+  <td> Authentication and Message Security in Cellular Networks</td>
+  <td>Jan 2015</td>
+</tr>
+<tr>
+  <td>3.</td>
+  <td>  Vipul Kumar Mishra</td>
+  <td>  Multi-Objective Design Space Exploration in High Level Synthesis for Application Specific Computing </td>
+  <td>July 2015</td>
+</tr>
+<tr>
+  <td>4.</td>
+  <td>Arpit Bharadwaj</td>
+  <td>Investigation in Tree based Genetic Programming for Data Classification</td>
+  <td>  December 2015</td>
+</tr>
+<tr>
+  <td>5.</td>
+  <td>Saumya Bhadauria  </td>
+  <td>Low Cost Fault Reliability and Trojan Security Aware High Level Synthesis for Application Specific Datapath Processors  </td>
+  <td>April 2016</td>
+</tr>
+<tr>
+  <td>6.</td>
+  <td>Neha Bharill </td>
+  <td>Supervised and Unsupervised Learning  Jan 2018</td>
+  <td>Jan 2018</td>
+</tr>
+<tr>
+  <td>7.</td>
+  <td>Robin Singh Bhadoria  </td>
+  <td>Assessment of Data and Interfacing of Service-Oriented Systems with Sensors </td>
+  <td>Jan 2018</td>
+</tr>
 
 
-<tr><td>Software Engineering                    </td> <td> 
-<a href="https://sites.google.com/site/asrivastavaiiti/" target="_newTab">Dr. Abhishek Srivastava</a>
-</td></tr>
-<tr><td>Dynamic Systems                         </td> <td>                           </td></tr>
-<tr><td>Model-Based Software Testing            </td> <td>                           </td></tr>
-<tr><td>Software Quality Assurance              </td> <td>                           </td></tr>
-<tr><td>Web Services                            </td> <td>                           </td></tr>
-<tr><td>			                            </td> <td>                          </td></tr>
 
-<tr><td>CAD-VLSI, EDA, High Level Synthesis, IP core Security, Hardware Trojan, </td><td>
-<a href="http://people.iiti.ac.in/~asengupt" target="_newTab">Dr. Anirban Sengupta</a> 
-</td></tr>
-<tr><td>Fault Security, Digital Watermark in digital chip, Optimization of Hardware Accelerators, Design Automation</td><td>                     </td></tr>
-<tr>
-  <td>Computer Architecture </td>
-  <td></td>
-</tr>
-<tr>
-  <td>Application Specific Processor Design from ESL TO RTL </td>
-  <td></td>
-</tr>
-<tr>
-  <td>Automation and Optimization of Custom Processors </td>
-  <td></td>
-</tr>
-<tr>
-  <td>High Level Synthesis/Electronic System Level Synthesis of Data Flow Graphs </td>
-  <td></td></tr>
-<tr><td>			                                          </td> <td>                    </td></tr>
 
-<tr><td>Genetic Algorithms/Evolutionary Approaches </td><td> 
-<a href="http://people.iiti.ac.in/~artiwari/" target = "_newTab">Dr. Aruna Tiwari</a>, 
-<a href="http://people.iiti.ac.in/~asengupt" target="_newTab">Dr. Anirban Sengupta</a>  </td></tr>
-<tr><td>			                               </td> <td>                           </td></tr>
-
-<tr><td>Soft Computing                      </td><td>
-<a href="http://people.iiti.ac.in/~artiwari/" target = "_newTab">Dr. Aruna Tiwari</a>
-</td></tr>
-<tr><td>Neural Network Learning Algorithms </td><td>                </td></tr>
-<tr><td>Artificial Intellignce             </td><td>                </td></tr>
-<tr><td>Databases                          </td><td>                </td></tr>
-<tr><td>			                       </td> <td>               </td></tr>
-
-<tr><td>Pattern Recognition                </td><td>
-<a href="http://people.iiti.ac.in/~surya" target="_newTab">Dr. Surya Prakash</a>, 
-<a href="http://people.iiti.ac.in/~somnathd" target="_newTab">Dr. Somnath Dey</a>
-</td></tr>
-<tr><td>Biometrics                         </td><td>                                  </td></tr>
-<tr><td>Computer Vision                    </td><td>                                  </td></tr>
-<tr><td>Image Processing                   </td><td>                                  </td></tr>
-<tr><td>Human Computer Interaction         </td><td>                                  </td></tr>
-<tr><td>Computer Graphics                  </td><td>                                  </td></tr>
-<tr><td>			                       </td> <td>                                  </td></tr>
-
-<tr><td>Numerical Analysis                    </td><td> <a href="http://iiti.ac.in/people/~kahuja/" target="_newTab"> Dr. Kapil Ahuja</a> </td></tr>
-<tr><td>Numerical Linear Algebra              </td><td>                  </td></tr>
-<tr><td>Computational Science and Engineeeing </td><td>                  </td></tr>
-<tr>
-  <td>Information Retrieval </td>
-  <td></td>
-</tr>
-<tr>
-  <td>				</td>  <td>				</td>		</tr>
-
-<tr><td> Formal Analysis of Reactive Systems: Model Checking, Abstract Interpretation,<br>
-Static Analysis                    </td><td> <a href="http://iitiiot.bitbucket.org/index.html" target="_newTab"> Dr. Gourinath Banda</a>
-<tr><td>Real Time Systems: Kernels, Software Engineering, RTOS, etc.</td><td>					</td></tr>
-<tr><td>Cyber-physical Systems, Embedded Systems, Mechatronic Systems..</td><td>				</td></tr>
-<tr>
-  <td>			</td>  <td>			</td>		</tr>
-<tr>
-  <td>Network Security  Cloud Security, </td>
-  <td><a href="http://iiti.ac.in/people/~neminath/">Dr. Neminath Hubballi&nbsp; </a></td>
-</tr>
-<tr>
-  <td>System Security</td>
-  <td></td>
-</tr>
-<tr>
-  <td>Dependable Systems and Data Mining.&nbsp;</td>
-  <td></td>
-</tr>
-<tr>
-  <td></td>
-  <td></td>
-</tr>
-<tr>
-  <td>Side-channel attacks, &nbsp;Fault analysis attacks,</td>
-  <td>Dr. Bodhisatwa Mazumdar</td>
-</tr>
-<tr>
-  <td>Fault sensitivity analysis of cryptographic implementations</td>
-  <td></td>
-</tr>
-<tr>
-  <td>Hardware security (Logic locking, IC camouflaging, IP security),</td>
-  <td></td>
-</tr>
-<tr>
-  <td>Logic synthesis and cryptographic implementations with improved resilience against side-channel attacks.</td>
-  <td></td>
-</tr>
 </table>
 </div>
 </div>
